5a7699b421728c9cb1683973d13be792eaad1531
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-10-07 22:19:14 +0200

    Added cli args for selecting generator

--- src/main/java/net/lenni0451/sourcegen/Main.java
+++ src/main/java/net/lenni0451/sourcegen/Main.java
@@ -1,12 +1,49 @@
 package net.lenni0451.sourcegen;
 
+import net.lenni0451.sourcegen.targets.GeneratorTarget;
 import net.lenni0451.sourcegen.targets.impl.MinecraftMojangMappings;
+
+import java.util.List;
 
 public class Main {
 
+    private static final List<GeneratorTarget> TARGETS = List.of(
+            new MinecraftMojangMappings()
+    );
+
     public static void main(String[] args) throws Throwable {
-        new MinecraftMojangMappings().execute();
-        System.out.println("Execution finished!");
+        if (args.length != 1) {
+            System.out.println("Please specify a target to generate!");
+            printTargets();
+            return;
+        }
+
+        int targetIndex;
+        try {
+            targetIndex = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid target index: " + args[0]);
+            printTargets();
+            return;
+        }
+        if (targetIndex < 1 || targetIndex > TARGETS.size()) {
+            System.out.println("Invalid target index: " + targetIndex);
+            printTargets();
+            return;
+        }
+
+        GeneratorTarget target = TARGETS.get(targetIndex - 1);
+        System.out.println("Generating target: " + target.getName());
+        target.execute();
+        System.out.println("Done!");
+    }
+
+    private static void printTargets() {
+        System.out.println("Available targets:");
+        for (int i = 0; i < TARGETS.size(); i++) {
+            GeneratorTarget target = TARGETS.get(i);
+            System.out.println((i + 1) + ". " + target.getName());
+        }
     }
 
 }
