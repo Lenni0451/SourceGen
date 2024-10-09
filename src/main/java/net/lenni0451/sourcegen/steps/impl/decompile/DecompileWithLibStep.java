707dfee7c62bcc81e365755ee09af618a8bb74b3
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-10-09 21:23:19 +0200

    Added steps required for cosmic reach

---
+++ src/main/java/net/lenni0451/sourcegen/steps/impl/decompile/DecompileWithLibStep.java
@@ -0,0 +1,30 @@
+package net.lenni0451.sourcegen.steps.impl.decompile;
+
+import net.lenni0451.sourcegen.steps.GeneratorStep;
+import net.lenni0451.sourcegen.utils.external.Commands;
+
+import java.io.File;
+
+public class DecompileWithLibStep implements GeneratorStep {
+
+    private final File input;
+    private final File library;
+    private final File output;
+
+    public DecompileWithLibStep(final File input, final File library, final File output) {
+        this.input = input;
+        this.library = library;
+        this.output = output;
+    }
+
+    @Override
+    public void printStep() {
+        System.out.println("Decompiling using VineFlower...");
+    }
+
+    @Override
+    public void run() throws Exception {
+        Commands.Vineflower.decompile(this.input, this.library, this.output);
+    }
+
+}
---
+++ src/main/java/net/lenni0451/sourcegen/steps/impl/io/ModifyJarStep.java
@@ -0,0 +1,34 @@
+package net.lenni0451.sourcegen.steps.impl.io;
+
+import net.lenni0451.sourcegen.steps.GeneratorStep;
+import net.lenni0451.sourcegen.utils.JarUtils;
+
+import java.io.File;
+import java.util.Map;
+import java.util.function.Predicate;
+
+public class ModifyJarStep implements GeneratorStep {
+
+    private final File input;
+    private final File output;
+    private final Predicate<String> filter;
+
+    public ModifyJarStep(final File input, final File output, final Predicate<String> removeEntry) {
+        this.input = input;
+        this.output = output;
+        this.filter = removeEntry;
+    }
+
+    @Override
+    public void printStep() {
+        System.out.println("Modifying jar file " + this.input.getName() + "...");
+    }
+
+    @Override
+    public void run() throws Exception {
+        Map<String, byte[]> entries = JarUtils.read(this.input);
+        entries.entrySet().removeIf(entry -> this.filter.test(entry.getKey()));
+        JarUtils.write(this.output, entries);
+    }
+
+}
