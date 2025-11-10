a31ef3fbab62cabaae8201c05805cda4ef8674a4
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-11-10 19:30:43 +0100

    Updated http client library

--- src/main/java/net/lenni0451/sourcegen/utils/NetUtils.java
+++ src/main/java/net/lenni0451/sourcegen/utils/NetUtils.java
@@ -19,16 +19,16 @@ public class NetUtils {
             .setHeader("User-Agent", "Mozilla/5.0");
 
     public static byte[] get(final String url) throws IOException {
-        return HTTP_CLIENT.get(url).execute().getContent();
+        return HTTP_CLIENT.get(url).execute().getContent().getAsBytes();
     }
 
     public static byte[] postInsecure(final String url, final String contentType, final byte[] data) throws IOException {
-        return HTTP_CLIENT.post(url).setContent(new ByteArrayContent(data)).setIgnoreInvalidSSL(true).setHeader(HttpHeaders.CONTENT_TYPE, contentType).execute().getContent();
+        return HTTP_CLIENT.post(url).setContent(new ByteArrayContent(data)).setIgnoreInvalidSSL(true).setHeader(HttpHeaders.CONTENT_TYPE, contentType).execute().getContent().getAsBytes();
     }
 
     public static void download(final String url, final File out) throws IOException {
         HttpResponse response = HTTP_CLIENT.get(url).setStreamedResponse(true).execute();
-        try (InputStream download = response.getInputStream()) {
+        try (InputStream download = response.getContent().getAsStream()) {
             if (response.getStatusCode() / 100 != 2) {
                 throw new IOException("Failed to download file from " + url + " (Status: " + response.getStatusCode() + ")");
             }
